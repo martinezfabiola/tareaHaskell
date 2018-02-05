a2c56f2013eaa7e5df49e929c1d9c8600a45ef8a
martinezfabiola <fabimartinezperez@gmail.com>
2018-02-05 11:22:08 -0400

    Arbol Binario

--- tarea1.hs
+++ tarea1.hs
@@ -34,14 +34,19 @@ validate tarjetadeCredito
   | otherwise = False
 -- validate tarjetadeCredito = sumDigitos (duplicarCadaOtro (aDigitosRev tarjetadeCredito)) `mod` 10 == 0
 
-data ArbBinario = L | N BinTree Bintree deriving (Eq, Show)
+data ArbBinario = L | N BinTree Bintree | N1 Bintree deriving (Eq, Show) 
 
 -- Crea arbol binario completo de tamano 2(n+1)-1
 crearArbBinario 0 = L 
 crearArbBinario n = N (crearArbBinario(n-1))(crearArbBinario(n-1))
 
+hacerABinTree :: Integer -> Integer -> ArbBinario
+hacerABinTree s n 
+	| (2*n) + 1 <= s -> N (hacerABinTree s ((2*n)) (hacerABinTree s ((2*n) + 1))
+	| (2*n) <=  s -> N1 (hacerABinTree((2*n))
+	| otherwise L
+
 -- Calcula el tamano del arbol 
 tam L = 1
 tam (N t1 t2) = 1 + tam t1 + tam t2
-
-
+tamanom (N1 t3) = 1 + tam t3
