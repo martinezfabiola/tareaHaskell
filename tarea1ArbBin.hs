120ddd235c54305d5c004f39f252dfdea20086b5
martinezfabiola <fabimartinezperez@gmail.com>
2018-02-05 17:29:10 -0400

    Modificaciones arb binario

--- tarea1ArbBin.hs
+++ tarea1ArbBin.hs
@@ -1,16 +1,35 @@
-data ArbBinario = L | N BinTree Bintree | N1 Bintree deriving (Eq, Show) 
+-- Universidad Simón Bolívar
+-- CI3661: Programming Languages I
+--
+-- Tarea 1 Haskell
+--
+-- Authors:
+--
+-- David Cabeza <13-10191@usb.ve>
+-- Fabiola Martínez <13-10838@usb.ve>
+--
+-- February, 2018.
 
--- Crea arbol binario completo de tamano 2(n+1)-1
-crearArbBinario 0 = L 
-crearArbBinario n = N (crearArbBinario(n-1))(crearArbBinario(n-1))
+-- Definimos el arbol binario
+data ArbBinario = Hoja
+                | N ArbBinario ArbBinario 
+                | N1 ArbBinario deriving (Eq, Show) 
 
-hacerABinTree s:: Integer -> Integer -> ArbBinario
-hacerABinTree s n 
-	| (2*n) + 1 <= s -> N (hacerABinTree s ((2*n)) (hacerABinTree s ((2*n) + 1))
-	| (2*n) <=  s -> N1 (hacerABinTree((2*n))
-	| otherwise L
+-- Funcion que crea arbol binario dado el tamano  
+hacerABinTree :: Integer -> Integer -> ArbBinario
+hacerABinTree tamano nodo
+ | (2*nodo) + 1 <= tamano = N (hacerABinTree tamano (2*nodo)) (hacerABinTree tamano ((2*nodo) + 1))
+ | 2*nodo <=  tamano = N1 (hacerABinTree  tamano (2*nodo))
+ | otherwise = Hoja
 
--- Calcula el tamano del arbol 
-tam L = 1
-tam (N t1 t2) = 1 + tam t1 + tam t2
-tamanom (N1 t3) = 1 + tam t3
+-- Funcion que calcula el tamano de un arbol binario
+calcularTamano :: ArbBinario  -> Integer
+calcularTamano Hoja = 1
+calcularTamano (N arbI arbD) = 1 + (calcularTamano arbI)+ (calcularTamano arbD)
+calcularTamano (N1 arb) = 1 + (calcularTamano arb)
+
+-- Funcion que calcula la profund de un arbol binario
+calcularProfundidad :: ArbBinario -> Integer
+calcularProfundidad Hoja = 1
+calcularProfundidad (N arb1 arb2) = 1 + max (calcularProfundidad arb1) (calcularProfundidad arb2)
+calcularProfundidad (N1 arb) = 1 + calcularProfundidad arb
