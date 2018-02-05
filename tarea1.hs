b11e1a05e7018921b17dbbd0d01fb9fc62bc077b
David Cabeza <cabezadavide@gmail.com>
2018-02-04 20:28:25 -0400

    chequear funciones

--- tarea1.hs
+++ tarea1.hs
@@ -12,12 +12,24 @@
 
 aDigitos :: Integer -> [Integer]
 aDigitos numTarjeta
-	-- numTarjeta is 0 or is negative then return []
-	| numTarjeta > 0 = aDigitos (numTarjeta `div` 10) ++ [numTarjeta `mod` 10]
-	| otherwise = []
+-- numTarjeta is 0 or is negative then return []
+  | numTarjeta > 0 = aDigitos (numTarjeta `div` 10) ++ [numTarjeta `mod` 10]
+  | otherwise = []
 
 aDigitosRev :: Integer -> [Integer]
 aDigitosRev = reverse . aDigitos
 
--- duplicarCadaOtro :: [Integer] -> [Integer]
--- duplicarCadaOtro = [x*2 | (index)]
+duplicarCadaOtro :: [Integer] -> [Integer]
+duplicarCadaOtro [] = []
+duplicarCadaOtro (primerDigito:[]) = [primerDigito]
+duplicarCadaOtro (primerDigito:segundoDigito:restoDigitos) = (primerDigito*2):segundoDigito:duplicarCadaOtro restoDigitos
+
+sumDigitos :: [Integer] -> Integer
+sumDigitos [] = 0
+sumDigitos (p:resto) = p + sumDigitos resto
+
+validate :: Integer -> Bool
+validate tarjetadeCredito
+  | (mod (sumDigitos (duplicarCadaOtro (aDigitosRev tarjetadeCredito))) 10 == 0) = True
+  | otherwise = False
+-- validate tarjetadeCredito = sumDigitos (duplicarCadaOtro (aDigitosRev tarjetadeCredito)) `mod` 10 == 0
